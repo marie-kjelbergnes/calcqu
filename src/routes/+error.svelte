a73b0877da369e88d58d30e91940e3ad1eef7227
marie <marie.kjelbergnes@gmail.com>
2026-06-12 16:14:31 +0200

    adressed #7

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<main>
+    <h1>Error {page.status}: {page.error?.message}</h1>
+    <p>Error URL: {page.url}</p>
+    <p><a href="./">Go home</a></p>
+</main>
+
+<style>
+    main {
+        display: flexbox;
+        justify-items: center;
+        margin: 2em;
+        flex-grow: 1;
+    }
+</style>
